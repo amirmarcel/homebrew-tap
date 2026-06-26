95c77c6b4d8f37ffcbb27dc15f96b6bcd0d7ffdd
Amir Marcel <amirmarcel@yahoo.com>
2026-06-25 20:04:34 -0400

    feat: update dotenv-linter to v0.2.0

--- Formula/dotenv-linter.rb
+++ Formula/dotenv-linter.rb
@@ -1,9 +1,9 @@
 class DotenvLinter < Formula
   desc "A CLI tool that diffs .env files against .env.example to catch missing or extra keys"
   homepage "https://github.com/amirmarcel/dotenv-linter"
-  url "https://github.com/amirmarcel/dotenv-linter/releases/download/v0.1.0/dotenv-linter_Darwin_arm64.tar.gz"
-  sha256 "f12a7c5dd288b44914a1729eab0e47c4b01e1ed2007da9e588675c8d678bedc0"
-  version "0.1.0"
+  url "https://github.com/amirmarcel/dotenv-linter/releases/download/v0.2.0/dotenv-linter_Darwin_arm64.tar.gz"
+  sha256 "4be13c4ffe51eb2378562916676d5378273a53d2a7aea36edd88c9163ce53b86"
+  version "0.2.0"
 
   def install
     bin.install "dotenv-linter"
